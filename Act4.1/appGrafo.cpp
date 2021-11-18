7c4e4b2bb4180889ac28a82e168749f39e12ff57
Jorge Leonardo <a01734836@itesm.mx>
2021-11-18 13:20:56 -0600

    Añadir esqueleto de 4.1

---
+++ Act4.1/appGrafo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void loadGraph(int n, int m, vector<vector<int>> &matAdj, vector<int> &listAdj){
+    
+}
+
+void DFS(vector<vector<int>> &matAdj,int inicial){
+    
+}
+
+void BFS(vector<int> &listAdj,int inicial){
+
+}
+
+int main(){
+
+
+    return 0;
+}
